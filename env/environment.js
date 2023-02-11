28ee1a709351b19990f0eb6abead11fde5f9d121
Pierre Gaimard <pierre@gaimard-family.fr>
2023-02-11 16:12:12 +0100

    set pm2 config

--- env/environment.js
+++ env/environment.js
@@ -1,8 +1,8 @@
-const path = require("path");
-
 module.exports = {
   dbURL: "", // MongoDB Atlas Database URL
   cert: "", // https certificate
   key: "", // server private key
   sessionSecret: "", // express-session secret
+  httpPort: 80, // http server port for https redirection.
+  httpsPort: 443, // https server port
 };
